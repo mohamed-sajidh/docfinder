94b16baf1b05497e63f7ec728a3160a5b2cb857f
sajidh <sajidhshaji4@gmail.com>
2025-07-12 11:53:41 +0530

    doctor details ui completed

--- lib/features/presentation/pages/single_doctor_list.dart
+++ lib/features/presentation/pages/single_doctor_list.dart
@@ -8,6 +8,7 @@ class SingleDoctorList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.black,
       appBar: AppBar(
         backgroundColor: AppColors.primary,
         centerTitle: true,
@@ -28,9 +29,97 @@ class SingleDoctorList extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Text("welcome to Single Doctor List"),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 210,
+            color: AppColors.grey,
+          ),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: AppColors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15,
+                  horizontal: 15,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 15),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Dr.Anagha akkru",
+                          style: TextStyle(
+                            color: AppColors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 22,
+                          ),
+                        ),
+                        Text(
+                          "Female",
+                          style: TextStyle(
+                            color: AppColors.grey,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 5),
+                    const Text(
+                      "pediatriacian",
+                      style: TextStyle(
+                        color: AppColors.grey,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    timeandLocation("Time", "10-29-2929"),
+                    const SizedBox(height: 5),
+                    timeandLocation("Location", "Palakkad"),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+Widget timeandLocation(String key, String value) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Text(
+        "$key:",
+        style: const TextStyle(
+          color: AppColors.black,
+          fontWeight: FontWeight.w500,
+          fontSize: 14,
+        ),
+      ),
+      Text(
+        value,
+        style: const TextStyle(
+          color: AppColors.black,
+          fontWeight: FontWeight.w500,
+          fontSize: 14,
+        ),
+      ),
+    ],
+  );
 }
